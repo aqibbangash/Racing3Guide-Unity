cdda0d581cd59f744d5d540500702018ebcc8792
Fahad Khan <fadikhan14@gmail.com>
2016-08-25 18:07:06 +0500

    initial commit-2

--- app/src/main/java/com/example/fahadkhan/realracingguide/Test.java
+++ app/src/main/java/com/example/fahadkhan/realracingguide/Test.java
@@ -2,15 +2,12 @@ package com.example.fahadkhan.realracingguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class Test extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Log.e("123","123");
+        setContentView(R.layout.activity_test);
     }
 }
