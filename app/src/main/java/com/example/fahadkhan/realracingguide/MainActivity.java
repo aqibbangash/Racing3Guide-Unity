2cc18ad671c4fb1e5c4ec9fc46497aaa6bfeec05
Fahad Khan <fadikhan14@gmail.com>
2016-08-29 17:28:20 +0500

    Full App with Ads

--- app/src/main/java/com/example/fahadkhan/realracingguide/MainActivity.java
+++ app/src/main/java/com/example/fahadkhan/realracingguide/MainActivity.java
@@ -6,11 +6,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.facebook.ads.Ad;
+import com.facebook.ads.AdError;
+import com.facebook.ads.AdSize;
+import com.facebook.ads.AdView;
+import com.facebook.ads.InterstitialAd;
+import com.facebook.ads.InterstitialAdListener;
 
 public class MainActivity extends AppCompatActivity {
 
     public  static String url;
     Button button,button1,button2,button3,button4,button5,button6;
+
+    AdView fbAdView;
+    public static InterstitialAd fbInterstitialAd, fbInterstitialAd2;
+
+    @Override
+    protected void onDestroy() {
+        fbInterstitialAd.destroy();
+        fbInterstitialAd2.destroy();
+        fbAdView.destroy();
+        super.onDestroy();
+    }
 
 
 
@@ -45,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 url="https://en.wikipedia.org/wiki/Doodle_Jump";
                 Intent intent = new Intent (MainActivity.this, webView.class);
                 startActivity(intent);
-                //fbInterstitialAd.show();
+                fbInterstitialAd.show();
 
             }
         });
@@ -55,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
                 url="http://www.gamerevolution.com/cheats/doodle-jump";
                 Intent intent = new Intent (MainActivity.this, webView.class);
                 startActivity(intent);
-                //fbInterstitialAd.show();
+                fbInterstitialAd.show();
 
             }
         });
@@ -65,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
                 url="http://www.getpcdownload.com/doodle-jump-pc-download/";
                 Intent intent = new Intent (MainActivity.this, webView.class);
                 startActivity(intent);
-                //fbInterstitialAd.show();
+                fbInterstitialAd.show();
             }
         });
         button4.setOnClickListener(new View.OnClickListener() {
@@ -75,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
                 url="http://appcheaters.com/doodle-jump-race-cheats-tricks/";
                 Intent intent = new Intent (MainActivity.this, webView.class);
                 startActivity(intent);
-                //fbInterstitialAd.show();
+                fbInterstitialAd.show();
             }
         });
 
@@ -85,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
                 url="http://www.wikihow.com/Be-Good-at-Doodle-Jump";
                 Intent intent = new Intent (MainActivity.this, webView.class);
                 startActivity(intent);
-                //fbInterstitialAd.show();
+                fbInterstitialAd.show();
 
             }
         });
@@ -96,11 +115,86 @@ public class MainActivity extends AppCompatActivity {
                 url="http://www.ehow.com/how_8674008_play-doodle-jump.html";
                 Intent intent = new Intent (MainActivity.this, webView.class);
                 startActivity(intent);
-                //fbInterstitialAd.show();
+                fbInterstitialAd.show();
             }
         });
-    }
 
+        RelativeLayout adViewContainer = (RelativeLayout) findViewById(R.id.adViewContainer);
+        fbAdView = new com.facebook.ads.AdView(this, "1568007433496063_1568007756829364", AdSize.BANNER_HEIGHT_50);
+        adViewContainer.addView(fbAdView);
+        fbAdView.loadAd();
+
+        fbInterstitialAd = new com.facebook.ads.InterstitialAd(this, "1568007433496063_1568469900116483");
+        fbInterstitialAd.setAdListener(new InterstitialAdListener() {
+            @Override
+            public void onInterstitialDisplayed(Ad ad) {
+
+            }
+
+            @Override
+            public void onInterstitialDismissed(Ad ad) {
+                fbInterstitialAd.loadAd();
+            }
+
+            @Override
+            public void onError(Ad ad, AdError adError) {
+
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+
+            }
+        });
+        fbInterstitialAd.loadAd();
+
+
+
+        fbInterstitialAd2 = new com.facebook.ads.InterstitialAd(this, "1568007433496063_1568469900116483");
+        fbInterstitialAd2.setAdListener(new InterstitialAdListener() {
+            @Override
+            public void onInterstitialDisplayed(Ad ad) {
+
+            }
+
+            @Override
+            public void onInterstitialDismissed(Ad ad) {
+                MainActivity.this.finish();
+            }
+
+            @Override
+            public void onError(Ad ad, AdError adError) {
+
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+
+            }
+        });
+        fbInterstitialAd2.loadAd();
+
+
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (fbInterstitialAd2.isAdLoaded()){
+            fbInterstitialAd2.show();
+        } else {
+            MainActivity.this.finish();
+        }
+    }
 
 }
 
